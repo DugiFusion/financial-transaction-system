a1da55549c06d13d2f0f42ae07261f8e200def66
Mirko Dugajlic <mirkodugi888gmail.com>
2024-12-05 15:32:05 +0100

    random change

--- Backend/Services/Transaction.API/Program.cs
+++ Backend/Services/Transaction.API/Program.cs
@@ -11,7 +11,7 @@ using Transactions.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
