a77cb1702398f0d594aaae5f90947bce2ccee0dd
Mirko Dugajlic <mirkodugi888@gmail.com>
2025-01-09 01:36:34 +0100

    updated reporting service on frontend

--- ClientApp/src/app/services/reporting/reporting.service.ts
+++ ClientApp/src/app/services/reporting/reporting.service.ts
@@ -11,12 +11,4 @@ export class ReportingService {
 
   private readonly apiUrl = environment.apiUrl;
   constructor(private readonly http: HttpClient) {}
-
-  generate(transactions: Transaction[]): Observable<Transaction[]> {
-    console.log('Transactions to generate:', transactions);
-
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-   // const message = JSON.stringify(transactions); 
-    return this.http.post<Transaction[]>(`${this.apiUrl}/transaction/send-message`, { transactions }, { headers });
-  }
 }
